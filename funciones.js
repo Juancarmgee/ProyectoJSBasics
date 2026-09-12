7fc3556ef6da53abe93e3d76cab909b25b2a8621
Juan Carlos Marquez Guevara <juancarmge.e@gmail.com>
2026-09-12 12:44:43 -0600

    funciones tema

--- funciones.js
+++ funciones.js
@@ -24,3 +24,50 @@ const firstFunction3 = function (func1){
     console.log (`Hola ${func1}`)
 }
 firstFunction3 ("Jcmg")
+
+// Arrow functions 
+
+const myfunc3 = (name) => {
+    console.log (`Hola ${name}`)
+}
+
+myfunc3 ("Juan Carlos")
+
+//Parametros 
+
+function sum (a , b) { 
+    console.log (a + b)
+}
+
+sum (34 , 56)
+
+
+// Retorno de valores
+
+function multi (a, b){
+    return a*b
+}
+    
+console.log (multi(2,5))
+
+// Funciones anidadas 
+
+function extern (){
+    console.log ("Funcion interna")
+    function intern (){
+        console.log ("Si esto se llama fuera del scoper de extern no se toma en cuenta, por que está dentro del scope, pero si se llama dentro de extern, si se se ejecuta")
+    }
+    intern()
+}
+
+extern ()
+
+//foreach 
+
+myArray = [1,2,3,4]
+
+myArray.forEach (function (value){
+    console.log(value)
+});
+
+myArray.forEach((value) => console.log (value))
